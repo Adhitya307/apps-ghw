11d15fd23c007a0d14ea626d23c00beae9a60233
Alifiya25 <alifiyaseftikaputri@gmail.com>
2025-08-27 06:29:40 +0700

    Menambah Button History

--- app/src/main/java/com/example/kerjapraktik/HomeActivity.java
+++ app/src/main/java/com/example/kerjapraktik/HomeActivity.java
@@ -2,13 +2,15 @@ package com.example.kerjapraktik;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private Button btnInput, btnInputHP2;
+    private LinearLayout btnInput, btnInputHP2, btnHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +20,35 @@ public class HomeActivity extends AppCompatActivity {
         // Inisialisasi tombol
         btnInput = findViewById(R.id.btnInput);       // Input HP 1
         btnInputHP2 = findViewById(R.id.btnInputHP2); // Input HP 2
+        btnHistory = findViewById(R.id.btnHistory);   // Data History
 
         // Tombol untuk HP 1 (input pengukuran dan lainnya)
-        btnInput.setOnClickListener(v -> {
-            Intent intent = new Intent(HomeActivity.this, InputDataActivity.class);
-            intent.putExtra("device_mode", "hp1"); // kirim mode (opsional)
-            startActivity(intent);
+        btnInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, InputDataActivity.class);
+                intent.putExtra("device_mode", "hp1"); // kirim mode (opsional)
+                startActivity(intent);
+            }
         });
 
-// Tombol untuk HP 2 (memilih pengukuran ID & input SR/Bocoran)
-        btnInputHP2.setOnClickListener(v -> {
-            Intent intent = new Intent(HomeActivity.this, InputData2Activity.class); // ← Ganti ke activity yang benar
-            intent.putExtra("device_mode", "hp2"); // opsional
-            startActivity(intent);
+        // Tombol untuk HP 2 (memilih pengukuran ID & input SR/Bocoran)
+        btnInputHP2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, InputData2Activity.class);
+                intent.putExtra("device_mode", "hp2"); // opsional
+                startActivity(intent);
+            }
+        });
+
+        // Tombol untuk Data History
+        btnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, HistoryActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
